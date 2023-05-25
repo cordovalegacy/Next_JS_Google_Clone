5b13f6a2846e2ff17ec3de02202341d1a7bf1635
Brendan Cordova <cordovalegacy19@gmail.com>
2023-05-25 15:55:48 -0700

    Working on search layout

---
+++ src/app/components/SearchBox.jsx
@@ -0,0 +1,8 @@
+
+
+const SearchBox = () => {
+    return(
+        <>Hello World</>
+    )
+}
+export default SearchBox
--- src/app/components/SearchNav.jsx
+++ src/app/components/SearchNav.jsx
@@ -1,8 +1,23 @@
+import Image from "next/image"
+import Link from "next/link"
+import SearchBox from "./SearchBox"
 
 
 const SearchNav = () => {
-    return(
-        <h2>Search Navigation</h2>
+    return (
+        <nav>
+            <header className="flex">
+                <Link href="/">
+                    <Image
+                        width={120}
+                        height={40}
+                        src={'/google_logo.svg'}
+                        alt="google logo"
+                    />
+                </Link>
+                <SearchBox />
+            </header>
+        </nav>
     )
 }
 
