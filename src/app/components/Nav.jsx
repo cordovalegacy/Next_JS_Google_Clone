0b6ba33e9864e1399096c30fe234d9936213b366
Brendan Cordova <cordovalegacy19@gmail.com>
2023-05-25 09:24:48 -0700

    Done with Nav, used Header for SEO

--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -1,8 +1,16 @@
-
+import Link from "next/link"
+import { TbGridDots } from 'react-icons/tb'
 
 const Nav = () => {
-    return(
-        <nav></nav>
+    return (
+        <nav className="p-5">
+            <header className="flex items-center justify-end gap-5">
+                <Link className="hover:underline" href={'https://mail.google.com/mail'}>Gmail</Link>
+                <Link className="hover:underline" href={'https://www.google.com/imghp?hl=en&tab=ri&ogbl'}>Images</Link>
+                <TbGridDots className="bg-transparent cursor-pointer hover:bg-gray-300 rounded-full p-1 text-3xl" />
+                <button className="bg-blue-500 text-white px-6 py-1 rounded-md hover:brightness-110 hover:shadow-md transition duration-150">Sign In</button>
+            </header>
+        </nav>
     )
 }
 
