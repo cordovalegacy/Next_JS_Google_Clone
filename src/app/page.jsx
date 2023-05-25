26859a796577e3fce62e959650525762522dcdd0
Brendan Cordova <cordovalegacy19@gmail.com>
2023-05-25 09:59:21 -0700

    Added google image, search bar w/ search icon and voice command button

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,39 @@
-import Nav from "./components/Nav"
+import Image from "next/image";
+import Nav from "./components/Nav";
+import { BsSearch } from 'react-icons/bs';
 
-export default function Home() { //use ctrl + space to initialize tailwind suggestions
+export default function Home() {
   return (
     <>
+      {/* header */}
       <Nav />
+      {/* body */}
+      <main className="w-full p-5 my-20 space-y-8">
+        <Image
+          width={300}
+          height={300}
+          src='/google_logo.svg'
+          alt="google logo"
+          className="mx-auto"
+        />
+        <form className="relative mx-auto w-2/5">
+          <BsSearch className="absolute top-1/2 left-2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 cursor-pointer" />
+          <input
+            type="text"
+            className="w-full pl-10 pr-14 py-2 rounded-full border border-gray-200 hover:shadow-md shadow-gray-600 bg-white"
+            placeholder="Search Google or type a URL"
+          />
+          <div className="absolute top-1/2 right-2 transform -translate-y-1/2">
+            <Image
+              src="/google_voice.svg"
+              alt="voice search"
+              width={20}
+              height={20}
+              className="hover:cursor-pointer hover:brightness-90"
+            />
+          </div>
+        </form>
+      </main>
     </>
-  )
+  );
 }
