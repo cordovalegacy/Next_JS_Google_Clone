2c2cad58fa0af2576afbaf83405e60492219b5db
Brendan Cordova <cordovalegacy19@gmail.com>
2023-05-26 13:06:12 -0700

    Finished search page

--- src/app/components/SearchBox.jsx
+++ src/app/components/SearchBox.jsx
@@ -1,8 +1,40 @@
+"use client"
+import Image from "next/image"
 
+const SearchBox = ({ googleSearch, setGoogleSearch, submitHandler, Search, searchParams, searchTerm }) => {
 
-const SearchBox = () => {
-    return(
-        <>Hello World</>
+    return (
+        <form onSubmit={submitHandler} className="relative mx-auto w-3/5">
+            <Search
+                className="absolute top-1/2 right-3 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 cursor-pointer"
+                onClick={submitHandler}
+            />
+            <input
+                type="text"
+                className="w-full pl-5 pr-14 py-2 rounded-full border border-gray-200 hover:shadow-md shadow-gray-600 bg-white"
+                placeholder="Search Google or type a URL"
+                onChange={(e) => setGoogleSearch(e.target.value)}
+                value={googleSearch}
+            />
+            {
+                googleSearch.length > 0 ?
+                    <span
+                        className="hover:text-black active:text-red-600 cursor-pointer text-sm px-5 absolute top-1/2 right-12 text-slate-500 transform -translate-y-1/2"
+                        onClick={() => setGoogleSearch("")}
+                    >X</span> :
+                    null
+            }
+            <div className="flex items-center absolute top-1/2 right-8 transform -translate-y-1/2">
+                <span className="text-slate-200 text-xl">|</span>
+                <Image
+                    src="/google_voice.svg"
+                    alt="voice search"
+                    width={20}
+                    height={20}
+                    className="hover:cursor-pointer hover:brightness-90"
+                />
+            </div>
+        </form>
     )
 }
 export default SearchBox
--- src/app/components/SearchNav.jsx
+++ src/app/components/SearchNav.jsx
@@ -1,21 +1,57 @@
+"use client"
+
 import Image from "next/image"
 import Link from "next/link"
 import SearchBox from "./SearchBox"
+import { RiSettings3Line as Settings } from 'react-icons/ri'
+import { TbGridDots as Grid } from 'react-icons/tb'
+import { useState } from "react"
+import { BsSearch as Search } from 'react-icons/bs'
+import { useSearchParams } from "next/navigation"
+import { useRouter } from "next/navigation"
 
 
 const SearchNav = () => {
+
+    const router = useRouter()
+    const searchParams = useSearchParams()
+    const searchTerm = searchParams.get("searchTerm")
+    const [googleSearch, setGoogleSearch] = useState(searchTerm || "")
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        if (!googleSearch.trim() /* trim eliminates trailing and leading whitespaces */) {
+          return //block empty submission
+        }
+        router.push(`/search/web?searchTerm=${googleSearch}`) //adds the path to the domain of the site (google.com)
+      }
+
     return (
-        <nav>
-            <header className="flex">
-                <Link href="/">
-                    <Image
-                        width={120}
-                        height={40}
-                        src={'/google_logo.svg'}
-                        alt="google logo"
+        <nav className="sticky-top-0">
+            <header className="flex items-center justify-between w-full p-6">
+                <div className="flex w-5/6 items-center">
+                    <Link href="/">
+                        <Image
+                            width={180}
+                            height={40}
+                            src={'/google_logo.svg'}
+                            alt="google logo"
+                        />
+                    </Link>
+                    <SearchBox 
+                        googleSearch={googleSearch}
+                        setGoogleSearch={setGoogleSearch}
+                        submitHandler={submitHandler}
+                        Search={Search}
+                        searchParams={searchParams}
+                        searchTerm={searchTerm}
                     />
-                </Link>
-                <SearchBox />
+                </div>
+                <div className="flex items-center justify-evenly w-1/6">
+                    <Settings className="header-icon hidden md:inline-flex" />
+                    <Grid className="header-icon hidden md:inline-flex" />
+                    <button className="bg-blue-500 text-white px-3 py-1 rounded-md hover:brightness-110 hover:shadow-md transition duration-150">Sign In</button>
+                </div>
             </header>
         </nav>
     )
