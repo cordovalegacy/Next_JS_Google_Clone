b6ac60ba7c4b3332397df783fe973fd542d3fcbd
Brendan Cordova <cordovalegacy19@gmail.com>
2023-05-26 16:45:53 -0700

    hooked up to search engine

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,12 +1,19 @@
 // !this component is used in the search/layout.jsx file as {children}
 
-const WebSearch = async() => {
+const WebSearch = async({ searchParams }) => {
 
-    const query = `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=lectures`
+    const query = `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
     const res = await fetch(query)
+    const data = await res.json()
+    // console.log("Data----->",data)
+    const results = data.items //data.items is an array
 
     return(
-        <h1>Web Search Page</h1>
+        <>
+        {results && results.map((res) => (
+            <h2>{res.title}</h2>
+        ))}
+        </>
     )
 }
 
