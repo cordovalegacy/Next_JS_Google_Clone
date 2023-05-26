8e1fb833631c8b5adfeb57e54bed529dad0921e1
Brendan Cordova <cordovalegacy19@gmail.com>
2023-05-26 16:32:07 -0700

    google search engine api key

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,6 +1,10 @@
 // !this component is used in the search/layout.jsx file as {children}
 
-const WebSearch = () => {
+const WebSearch = async() => {
+
+    const query = `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=lectures`
+    const res = await fetch(query)
+
     return(
         <h1>Web Search Page</h1>
     )
